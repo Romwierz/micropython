009a2cb51defa21762fc3847a77015a7fd2cf1e8
Romwierz <miczu47@gmail.com>
2026-09-16 15:14:43 +0200

    add some stub functions for hal interface

--- ports/zynq/mphalport.c
+++ ports/zynq/mphalport.c
@@ -1,5 +1,7 @@
 #include "py/mpconfig.h"
 #include "xuartps_hw.h"
+#include "py/runtime.h"
+#include "py/mphal.h"
 
 // Receive single character
 int mp_hal_stdin_rx_chr(void) {
@@ -16,3 +18,40 @@ mp_uint_t mp_hal_stdout_tx_strn(const char *str, mp_uint_t len) {
     }
     return ret;
 }
+
+mp_uint_t mp_hal_ticks_us(void) {
+    return 0;
+}
+
+mp_uint_t mp_hal_ticks_ms(void) {
+    return 0;
+}
+
+void mp_hal_delay_us(mp_uint_t us) {
+}
+
+void mp_hal_delay_ms(mp_uint_t ms) {
+    mp_uint_t start = mp_hal_ticks_ms();
+    mp_uint_t elapsed = 0;
+    do {
+        mp_event_wait_ms(ms - elapsed);
+        elapsed = mp_hal_ticks_ms() - start;
+    } while (elapsed < ms);
+}
+
+uintptr_t mp_hal_stdio_poll(uintptr_t poll_flags) {
+    uintptr_t ret = 0;
+    #if MICROPY_HW_USB_CDC
+    ret |= mp_usbd_cdc_poll_interfaces(poll_flags);
+    #endif
+    #if MICROPY_HW_ENABLE_UART_REPL
+    if (poll_flags & MP_STREAM_POLL_WR) {
+        ret |= MP_STREAM_POLL_WR;
+    }
+    #endif
+    #if MICROPY_PY_OS_DUPTERM
+    ret |= mp_os_dupterm_poll(poll_flags);
+    #endif
+    return ret;
+}
+
